f7f1e71c385ab21010399d1c87a47b6c738f7108
Terence Qu <Terenco12345@gmail.com>
2020-05-25 12:07:32 +1200

    Implemented the full landing page
    - Title and registration button
    - Body (description of everything)

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import styles from "./css/app.module.css";
 
+import{
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import LandingPage from "./components/landing/LandingPage";
 
 function App() {
   return (
     <div className = {styles.root}>
-      <LandingPage />
+      
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
