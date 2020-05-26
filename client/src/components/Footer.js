373a2af3750215ec2a763250927ba93b5b879de1
Terence Qu <Terenco12345@gmail.com>
2020-05-26 22:49:38 +1200

    Implement Website Footer
    Footer includes contact information, as well as social media links.

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -14,7 +14,12 @@ class Footer extends React.Component{
     render(){
         return (
             <div className={styles.footerRoot}>
-                
+                <h2>Contact us - Tell us if you have any bugs, issues or suggestions!</h2>
+                <div className={styles.socialMediaContainer}>
+                    <a href="https://www.facebook.com/" target="_blank" className="fa fa-facebook"></a>
+                    <a href="https://twitter.com/Cheese_Gr8er" target="_blank" className="fa fa-twitter"></a>
+                </div>
+                <h2>Email: Terenco12345@gmail.com</h2>
             </div>
         );
     }
